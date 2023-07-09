3851816c4bbf5bccdcc680bc115647170092a89b
Kalamun <info@kalamun.org>
2023-07-09 16:15:32 +0000

    Revert last wrong commit

--- classes/class.ilCardPluginGUI.php
+++ classes/class.ilCardPluginGUI.php
@@ -275,7 +275,7 @@ class ilCardPluginGUI extends ilPageComponentPluginGUI
                 </div>
                 <div class="kalamun-card_body">
                     <div class="kalamun-card_title">
-                        <a href="<?= $permalink; ?>"><?= $type.' ' .$title; ?></a>
+                        <a href="<?= $permalink; ?>"><?= $title; ?></a>
                     </div>
                     <?php
                     if (!empty($description)) { ?>
