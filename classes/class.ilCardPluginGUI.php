6d44e5457fbb5942dcadaab14eebd55edfcbea86
kalamun <rp@kalamun.net>
2023-11-02 14:29:35 +0000

    Support Jitsi

--- classes/class.ilCardPluginGUI.php
+++ classes/class.ilCardPluginGUI.php
@@ -272,6 +272,9 @@ class ilCardPluginGUI extends ilPageComponentPluginGUI
             $this->ctrl->setParameterByClass("ilrepositorygui", "ref_id", $ref_id);
             $permalink = $this->ctrl->getLinkTargetByClass("ilrepositorygui", "view");
             //$permalink = "/ilias.php?ref_id=" . $ref_id . "&cmd=view&cmdClass=ilrepositorygui&cmdNode=wl&baseClass=ilrepositorygui";
+        } elseif ($type == "xjit") {
+            $this->ctrl->setParameterByClass("ilobjcontentpagegui", "ref_id", $ref_id);
+            $permalink = $this->ctrl->getLinkTargetByClass("ilobjcontentpagegui", "view");
         } else {
             $this->ctrl->setParameterByClass("ilrepositorygui", "ref_id", $ref_id);
             $permalink = $this->ctrl->getLinkTargetByClass("ilrepositorygui", "view");
