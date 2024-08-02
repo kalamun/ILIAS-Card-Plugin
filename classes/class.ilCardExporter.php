3a8300222b53f3f63ffbff5df7b9d59697e1dd53
kalamun <rp@kalamun.net>
2024-08-02 16:00:01 +0000

    Improve exporter and importer

--- classes/class.ilCardExporter.php
+++ classes/class.ilCardExporter.php
@@ -6,6 +6,67 @@
  */
 class ilCardExporter extends ilXmlExporter
 {
+    public function getXmlExportHeadDependencies(/* string */ $a_entity, /* string */ $a_target_release, /* array */ $a_ids) /* : array */
+    {
+        // collect the files to export
+        // ref_id
+        // thumbnail
+        // https://docu.ilias.de/ilias.php?baseClass=illmpresentationgui&obj_id=32894&ref_id=42
+        // https://github.com/ILIAS-eLearning/TestPageComponent/blob/master/classes/class.ilTestPageComponentExporter.php
+
+        $ref_id = false;
+        $file_id = false;
+        foreach ($a_ids as $id) {
+            $properties = ilPageComponentPluginExporter::getPCProperties($id);
+            if (isset($properties['ref_id'])) {
+                $ref_id = $properties['ref_id'];
+            }
+            if (isset($properties['thumbnail'])) {
+                $file_id = $properties['thumbnail'];
+            }
+        }
+
+        $deps = [];
+
+        $components = [
+            "copa" => "Modules/ContentPage",
+            "lm" => "Modules/LearningModule",
+            "file" => "Modules/File",
+            "sahs" => "Modules/Scorm2004",
+            "htlm" => "Modules/HTMLLearningModule",
+            "tst" => "Modules/Test",
+            "fold" => "Modules/Folder",
+            "xjit" => "",
+            "exc" => "Modules/Exercise",
+            "frm" => "Modules/Forum",
+        ];
+
+        // add the files as dependencies
+        if (!empty(($ref_id))) {
+            $obj = ilObjectFactory::getInstanceByRefId($ref_id);
+            if (!empty($obj)) {
+                $obj_id = $obj->getId();
+                $type = $obj->getType();
+    
+                if (!empty($components[$type])) {
+                    $deps[] = array(
+                        "component" => $components[$type],
+                        "entity" => $type,
+                        "ids" => $ref_id
+                    );
+                }
+            }
+        }
+        if (!empty(($file_id))) {
+            $deps[] = array(
+                "component" => $components["file"],
+                "entity" => "file",
+                "ids" => $file_id
+            );
+        }
+
+        return $deps;
+    }
 
     /**
      * Get xml representation
@@ -20,7 +81,6 @@ class ilCardExporter extends ilXmlExporter
         $obj_id = intval(explode(":", $a_id)[1]);
         $ref_ids = ilObject::_getAllReferences($obj_id);
         $ref_id = array_shift($ref_ids);
-        var_dump($a_id, $obj_id, $ref_id); die();
 
         if (empty($ref_id)) return false;
 
@@ -43,6 +103,18 @@ class ilCardExporter extends ilXmlExporter
     public function init() : void
     {
         // TODO: Implement init() method.
+    }
+
+    /**
+     * Get tail dependencies
+     * @param string        entity
+     * @param string        target release
+     * @param array        ids
+     * @return        array        array of array with keys "component", entity", "ids"
+     */
+    public function getXmlExportTailDependencies(/* string */ $a_entity, /* string */ $a_target_release, /* array */ $a_ids) /* : array */
+    {
+        return array();
     }
 
     /**
--- classes/class.ilCardImporter.php
+++ classes/class.ilCardImporter.php
@@ -4,7 +4,7 @@
  * Class ilCardImporter
  * @author Oskar Truffer <ot@studer-raimann.ch>
  */
-class ilCardImporter extends ilXmlImporter
+class ilCardImporter extends ilPageComponentPluginImporter /* ilXmlImporter */
 {
     /**
      * Import xml representation
@@ -19,16 +19,47 @@ class ilCardImporter extends ilXmlImporter
         /* string */ $a_xml,
         /* ilImportMapping */ $a_mapping
     ) /* : void */ {
-      return false;
-        $xml = simplexml_load_string($a_xml);
-        $pl = new ilCardPlugin();
-        $entity = new ilObjCard();
-        $entity->setTitle((string) $xml->title . " " . $pl->txt("copy"));
-        $entity->setDescription((string) $xml->description);
-        $entity->setOnline((string) $xml->online);
-        $entity->setImportId($a_id);
-        $entity->create();
-        $new_id = $entity->getId();
-        $a_mapping->addMapping("Plugins/TestObjectRepository", "xtst", $a_id, $new_id);
+        global $DIC;
+
+        /** @var ilComponentFactory $component_factory */
+        // $component_factory = $DIC["component.factory"]; // ILIAS 8
+
+        /** @var ilTestPageComponentPlugin $plugin */
+        /* $plugin = $component_factory->getPlugin("pcard"); // ILIAS 8 */
+        $plugin = ilPluginAdmin::getPluginObject(IL_COMP_SERVICE, 'COPage', 'pgcp', 'Card');
+
+        $new_id = self::getPCMapping($a_id, $a_mapping);
+
+        $properties = self::getPCProperties($new_id);
+        $version = self::getPCVersion($new_id);
+
+        $components = [
+            "copa" => "Modules/ContentPage",
+            "lm" => "Modules/LearningModule",
+            "file" => "Modules/File",
+            "sahs" => "Modules/Scorm2004",
+            "htlm" => "Modules/HTMLLearningModule",
+            "tst" => "Modules/Test",
+            "fold" => "Modules/Folder",
+            "xjit" => "",
+            "exc" => "Modules/Exercise",
+            "frm" => "Modules/Forum",
+        ];
+
+        if ($old_file_id = $properties['ref_id']) {
+            foreach($components as $type => $component) {
+                $new_file_id = $a_mapping->getMapping($component, $type, $old_file_id);
+                if (!empty($new_file_id)) break;
+            }
+            $properties['ref_id'] = $new_file_id;
+        }
+
+        if ($old_file_id = $properties['thumbnail']) {
+            $new_file_id = $a_mapping->getMapping("Modules/File", 'file', $old_file_id);
+            $properties['thumbnail'] = $new_file_id;
+        }
+
+        self::setPCProperties($new_id, $properties);
+        self::setPCVersion($new_id, $version);
     }
 }
