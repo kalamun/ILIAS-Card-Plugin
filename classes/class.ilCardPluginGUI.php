087ece067c8f9086af9edd6b3d153b7399e166ab
kalamun <rp@kalamun.net>
2026-01-30 14:35:57 +0100

    Avoid crashing on no node_id given

--- classes/class.ilCardPluginGUI.php
+++ classes/class.ilCardPluginGUI.php
@@ -148,7 +148,9 @@ class ilCardPluginGUI extends ilPageComponentPluginGUI
         $form = new ilPropertyFormGUI();
 
         $root_course = dciSkin_tabs::getRootCourse($_GET['ref_id']);
-        $subTree = $this->tree->getSubTree($this->tree->getNodeData($root_course['ref_id']));
+        if (!empty($root_course['ref_id'])) {
+            $subTree = $this->tree->getSubTree($this->tree->getNodeData($root_course['ref_id']));
+        }
         
         $select_options = [];
         foreach($subTree as $obj) {
